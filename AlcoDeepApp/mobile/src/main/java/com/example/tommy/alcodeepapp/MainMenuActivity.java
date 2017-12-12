de68cfb7c8901ab7da500945d88cb5bee1a3d252
qholam <qholam@wpi.edu>
2017-12-11 19:48:31 -0500

    implemented Message API

--- AlcoDeepApp/mobile/src/main/java/com/example/tommy/alcodeepapp/MainMenuActivity.java
+++ AlcoDeepApp/mobile/src/main/java/com/example/tommy/alcodeepapp/MainMenuActivity.java
@@ -1,37 +1,74 @@
 package com.example.tommy.alcodeepapp;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainMenuActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.CapabilityApi;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
+
+public class MainMenuActivity extends AppCompatActivity implements
+        SensorEventListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        MessageApi.MessageListener{
+    private GoogleApiClient mGoogleApiClient;
+    private String DATA_RECEIVER_CAPABILITY = "data_receiver";
+    private String DATA_RECEIVER_PATH = "/data_receiver";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        //Build a new GoogleApiClient
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
         disableStopButton();
     }
 
     public void pressStartButton(View v) {
+        //disable start button and enable stop button
         disableStartButton();
         enableStopButton();
 
+        //update status
         TextView status = (TextView) findViewById(R.id.status);
         status.setText("Recording...");
+
+        //start main activity on watch
+        startWatch();
     }
 
     public void pressStopButton(View v) {
+        //disable stop button and enable start button
         disableStopButton();
         enableStartButton();
 
+        //update status
         TextView status = (TextView) findViewById(R.id.status);
         status.setText("Stopped...");
 
+        //stop main activity on watch
+        stopWatch();
+
+        //go to result activity
         Intent intent = new Intent(this, ResultActivity.class);
         startActivity(intent);
     }
@@ -54,5 +91,74 @@ public class MainMenuActivity extends AppCompatActivity {
     private void disableStopButton() {
         Button btn = (Button) findViewById(R.id.stopButton);
         btn.setEnabled(false);
+    }
+
+    private void startWatch() {
+        sendStringToWatch("start");
+    }
+
+    private void stopWatch() {
+        sendStringToWatch("stop");
+    }
+
+    private void sendStringToWatch(String str) {
+        new SendToWatch(DATA_RECEIVER_PATH, str).start();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        https://github.com/nhaarman/supertooltips
+    }
+
+    class SendToWatch extends Thread {
+        String path;
+        String data;
+
+        SendToWatch(String p, String d) {
+            path = p;
+            data = d;
+        }
+
+        public void run() {
+            //retrieve watch node
+            CapabilityApi.GetCapabilityResult result =
+                    Wearable.CapabilityApi.getCapability(
+                            mGoogleApiClient,  DATA_RECEIVER_CAPABILITY,
+                            CapabilityApi.FILTER_REACHABLE).await();
+
+            if(result.getCapability().getNodes().isEmpty())
+                return;
+
+            String watchNodeId = result.getCapability().getNodes().iterator().next().getId();
+
+            //send message to watch, telling it to start the recording process
+            Wearable.MessageApi.sendMessage(mGoogleApiClient, watchNodeId,
+                    DATA_RECEIVER_PATH, data.getBytes());
+        }
     }
 }
--- AlcoDeepApp/wear/src/main/java/com/example/tommy/alcodeepapp/MainActivity.java
+++ AlcoDeepApp/wear/src/main/java/com/example/tommy/alcodeepapp/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.tommy.alcodeepapp;
 
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.BoxInsetLayout;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class MainActivity extends WearableActivity {
+public class MainActivity extends WearableActivity implements
+        SensorEventListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        MessageApi.MessageListener{
 
     private static final SimpleDateFormat AMBIENT_DATE_FORMAT =
             new SimpleDateFormat("HH:mm", Locale.US);
@@ -20,6 +36,11 @@ public class MainActivity extends WearableActivity {
     private TextView mLogoView;
     private TextView mStatusView;
     private TextView mClockView;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    private String DATA_RECEIVER_CAPABILITY = "data_receiver";
+    private String DATA_RECEIVER_PATH = "/data_receiver";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +52,16 @@ public class MainActivity extends WearableActivity {
         mLogoView = (TextView) findViewById(R.id.text);
         mStatusView = (TextView) findViewById(R.id.status);
         mClockView = (TextView) findViewById(R.id.clock);
+
+        //create new GoogleApiClient
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        //set up listener for MessageApi
+        Wearable.MessageApi.addListener(mGoogleApiClient, this);
     }
 
     @Override
@@ -64,6 +95,49 @@ public class MainActivity extends WearableActivity {
             mLogoView.setTextColor(Color.BLACK);
             mStatusView.setTextColor(Color.BLACK);
             mClockView.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        if(messageEvent.getPath().equals(DATA_RECEIVER_PATH)) {
+            String data = new String(messageEvent.getData());
+            System.out.println(data);
+            switch(data) {
+                case "start":
+                    mStatusView.setText("Recording...");
+                    break;
+                case "stop":
+                    mStatusView.setText("Idle...");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
